ba4a5d2a94e2cd125e09b3985fa5229bff15ddd2
rakhmetov <rakhmetov@megalabs.ru>
2016-12-05 13:40:13 +0300

    file wsdl fix

--- src/main/java/ru/soap/test/client/SoapClient.java
+++ src/main/java/ru/soap/test/client/SoapClient.java
@@ -1,15 +1,26 @@
 package ru.soap.test.client;
 
 
-import hello.wsdl.GetCountryRequest;
-import hello.wsdl.GetCountryResponse;
+import jaxb.generated.GetCountryRequest;
+import jaxb.generated.GetCountryResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.oxm.jaxb.Jaxb2Marshaller;
+import org.springframework.stereotype.Repository;
 import org.springframework.ws.client.core.support.WebServiceGatewaySupport;
 import org.springframework.ws.soap.client.core.SoapActionCallback;
 
 /**
  * Created by rakhmetov on 05.12.16.
  */
+@Repository
 public class SoapClient extends WebServiceGatewaySupport {
+
+    @Autowired
+    public SoapClient(Jaxb2Marshaller marshaller) {
+        setDefaultUri("http://localhost:8080/ws");
+        setMarshaller(marshaller);
+        setUnmarshaller(marshaller);
+    }
 
     public GetCountryResponse getResponse(String name) {
 
@@ -20,6 +31,6 @@ public class SoapClient extends WebServiceGatewaySupport {
                 .marshalSendAndReceive(
                         "http://localhost:8080/ws/countries.wsdl",
                         request,
-                        new SoapActionCallback("http://ws.cdyne.com/WeatherWS/GetCityForecastByZIP"));
+                        new SoapActionCallback("http://localhost:8080/ws"));
     }
 }
--- src/main/java/ru/soap/test/config/TestConfiguration.java
+++ src/main/java/ru/soap/test/config/TestConfiguration.java
@@ -3,7 +3,6 @@ package ru.soap.test.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.oxm.jaxb.Jaxb2Marshaller;
-import ru.soap.test.client.SoapClient;
 
 /**
  * Created by rakhmetov on 05.12.16.
@@ -14,17 +13,7 @@ public class TestConfiguration {
     @Bean
     public Jaxb2Marshaller marshaller() {
         Jaxb2Marshaller marshaller = new Jaxb2Marshaller();
-        marshaller.setContextPath("hello.wsdl");
+        marshaller.setContextPath("jaxb.generated");
         return marshaller;
     }
-
-    @Bean
-    public SoapClient weatherClient(Jaxb2Marshaller marshaller) {
-        SoapClient client = new SoapClient();
-        client.setDefaultUri("http://localhost:8080/ws");
-        client.setMarshaller(marshaller);
-        client.setUnmarshaller(marshaller);
-        return client;
-    }
-
 }
